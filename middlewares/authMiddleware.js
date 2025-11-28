6af461c507a9039d4bde06ff1d447ef10e69b5d7
x-alpha319 <okebube13@gmail.com>
2025-11-28 22:37:02 +0100

    file name change

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../Models/User.js";
+import User from "../Models_temp/User.js";
 
 export const protectRoute = async (req, res, next) => {
   let token;
