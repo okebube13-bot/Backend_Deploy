6d5687d1718f67a02becf8eda2dbe4126f662ed1
x-alpha319 <okebube13@gmail.com>
2025-11-29 09:50:15 +0100

    Cors edit

--- app.js
+++ app.js
@@ -25,7 +25,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "https://frontend-deploy-mpdk.onrender.com",
+];
 
 app.use(morgan("dev"));
 app.use(
